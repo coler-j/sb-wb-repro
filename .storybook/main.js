22ab96dc492535d3405234d306aaf2d6a5b4522b
Cole Jancsar <njancsar@gmail.com>
2022-10-24 14:13:14 -0400

    wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const util = require("util");
+const _ = require("lodash");
 
 const appConfig = require("../webpack.config")({
   NODE_ENV: "development",
@@ -12,21 +13,50 @@ module.exports = {
   },
   framework: "@storybook/react",
   stories: ["../**/*.stories.@(tsx|mdx)"],
-  addons: ["@storybook/addon-docs"],
+  addons: [{
+    name: '@storybook/addon-docs',
+    options: {
+      configureJSX: true,
+      babelOptions: {},
+      sourceLoaderOptions: null,
+      transcludeMarkdown: true,
+    },
+  },],
   webpackFinal: async (config) => {
-    // loader incorrectly injects preview-head.html content in preview iframe so remove it
-    appConfig.module.rules = appConfig.module.rules.filter(({ test }) => !test.test(".ejs"));
+    // // loader incorrectly injects preview-head.html content in preview iframe so remove it
+    // appConfig.module.rules = appConfig.module.rules.filter(({ test }) => !test.test(".ejs"));
 
-    // remove svg loader from Storybook config and use webpack one from appConfig
-    config.module.rules = config.module.rules.filter(({ test }) => !test.test(".svg"));
+    // // remove svg loader from Storybook config and use webpack one from appConfig
+    // config.module.rules = config.module.rules.filter(({ test }) => !test.test(".svg"));
 
-    config.module.rules.push(...appConfig.module.rules);
+    // config.module.rules.push(...appConfig.module.rules);
 
-    config.resolve = appConfig.resolve;
-    config.resolve.extensions.push(".md");
-    config.resolve.alias = {
-      path: require.resolve("path-browserify"),
-    };
+    // config.resolve = appConfig.resolve;
+    // config.resolve.extensions.push(".md");
+    // config.resolve.alias = {
+    //   path: require.resolve("path-browserify"),
+    // };
+
+    const styleLoaders = appConfig.module.rules.filter(({ test }) => test.test(".scss"));
+
+    config = {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        extensions: _.union(appConfig.resolve.extensions, config.resolve.extensions),
+        modules: _.union(appConfig.resolve.modules, config.resolve.modules),
+      },
+      module: {
+        ...config.module,
+
+        rules: [
+          ...config.module.rules,
+          ...styleLoaders,
+        ]
+      }
+    }
+
+    console.log(util.inspect(config, false, null, true /* enable colors */));
 
     return config;
   },
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,5 +2,14 @@ import React from "react";
 import "common/polyfills/polyfills.full";
 import "focus-visible";
 import { MemoryRouter } from "react-router";
+import { addParameters } from '@storybook/react';
+import { DocsPage, DocsContainer } from '@storybook/addon-docs';
+
+addParameters({
+  docs: {
+    container: DocsContainer,
+    page: DocsPage,
+  },
+});
 
 export const decorators = (Story) => <MemoryRouter>{Story()}</MemoryRouter>;
