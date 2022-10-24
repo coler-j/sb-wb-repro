aba9dbdc726002cd22d4a824c9bae47fddd53612
Cole Jancsar <njancsar@gmail.com>
2022-10-24 14:52:40 -0400

    wip

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,14 +2,13 @@ import React from "react";
 import "common/polyfills/polyfills.full";
 import "focus-visible";
 import { MemoryRouter } from "react-router";
-import { addParameters } from '@storybook/react';
 import { DocsPage, DocsContainer } from '@storybook/addon-docs';
 
-addParameters({
+export const parameters = {
   docs: {
     container: DocsContainer,
     page: DocsPage,
   },
-});
+};
 
 export const decorators = (Story) => <MemoryRouter>{Story()}</MemoryRouter>;
